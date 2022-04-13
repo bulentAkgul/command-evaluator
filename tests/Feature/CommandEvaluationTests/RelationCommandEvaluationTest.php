8f1ecc2b6d7772a7253caaadc57225d3f1694b31
Bulent Akgul <bulent.akgul@me.com>
2022-04-13 23:06:31 +0300

    empty test classes deleted

--- tests/Feature/CommandEvaluationTests/RelationCommandEvaluationTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Bakgul\Evaluator\Tests\Feature\CommandEvaluationTests;
-
-use Bakgul\Evaluator\Tests\EvaluatorTestMethods;
-
-class RelationCommandEvaluationTest extends EvaluatorTestMethods
-{}
