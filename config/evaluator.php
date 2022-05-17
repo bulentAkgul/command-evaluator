227c7b60ee98d1efd8c4fc709828779c20be6c78
Bulent Akgul <bulentakgul1980@gmail.com>
2022-05-17 11:27:07 +0300

    config comments improved

--- config/evaluator.php
+++ config/evaluator.php
@@ -7,11 +7,11 @@ return [
     |--------------------------------------------------------------------------
     |
     | You can disable the evaluators, but it's definitely not recommended.
-    | Since the commands deal with the file system, you may breake your
-    | app, in the case of calling a command with some wrong settings.
-    | If you choose disabling the evaluator, use "file-history" package
-    | to rollback prior state of your app. On the other hand, disabling
-    | the warnings is perfectly safe after you get fimiliar with them.
+    | Since the commands deal with the file system, you may break your app
+    | when calling a command with some wrong settings. If you choose to
+    | disable the evaluator, use the "file-history" package to roll back
+    | the last state of your app. On the other hand, disabling the warnings
+    | is perfectly safe after getting familiar with them.
     |
     */
     'evaluate_commands' => true,
